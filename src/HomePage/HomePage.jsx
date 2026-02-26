c094c3106d679505cafd152202e6c820ffc51d48
Abhay-S-R <abhaysr946@gmail.com>
2026-02-26 06:05:59 +0000

    add timezone, sunrise and sunset

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,15 +1,16 @@
 import "./HomePage.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import WeatherDisplay from "../components/WeatherDisplay";
+import { toLocalTime } from "../utils/localTime.js";
 
 function HomePage() {
-  const [locInput, setLocInput] = useState(""); 
+  const [locInput, setLocInput] = useState("");
   const [weather, setWeather] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-
+  
   const handleSearch = async (city) => {
-    if(!city.trim()){
+    if (!city.trim()) {
       setError("Please enter a city name");
       return;
     }
@@ -18,25 +19,42 @@ function HomePage() {
     setWeather(null);
     setLoading(true);
 
-    try{
+    try {
       const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-      
-      const response = await fetch(url);
+      const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+      const geoResponse = await fetch(geoUrl);
+      const geoData = await geoResponse.json();
+      console.log(geoData);
 
-      if(!response.ok){
-        if(response.status === 404) throw new Error("City not found please try again");
+      if(!geoResponse.ok) throw new Error("Failed to fetch geo data");
+      if(geoData.length === 0) throw new Error("City not found")
+
+      const {lat, lon, name} = geoData[0];
+
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+      const weatherResponse = await fetch(weatherUrl);
+
+      if (!weatherResponse.ok) {
+        if (weatherResponse.status === 404)
+          throw new Error("City not found please try again");
         else throw new Error("Failed to fetch weather data");
       }
 
-      const data = await response.json();
+      //All API responses are in seconds
+      const data = await weatherResponse.json();
+      console.log(data);
+      const timeSecs = data.timezone / 3600;
 
       const weatherData = {
-        city: data.name,
+        city: name,
         temp: Math.round(data.main.temp),
         description: data.weather[0].description,
         feelsLike: Math.round(data.main.feels_like),
-        humidity: data.main.humidity
+        humidity: data.main.humidity,
+        timeSecs: data.timezone,
+        timeZone: timeSecs >= 0 ? `UTC+${timeSecs}` : `UTC${timeSecs}`,
+        sunrise: toLocalTime(data.sys.sunrise * 1000, data.timezone * 1000),
+        sunset: toLocalTime(data.sys.sunset * 1000, data.timezone * 1000),
       };
 
       setWeather(weatherData);
@@ -47,7 +65,6 @@ function HomePage() {
     } finally {
       setLoading(false);
     }
-
   };
 
   return (
---
+++ src/utils/localTime.js
@@ -0,0 +1,9 @@
+//uEverything is in milliseconds
+export function toLocalTime(unixTime, timeMilliSecs, showSeconds=false){
+  const date = new Date(unixTime+timeMilliSecs);
+  const hours = String(date.getUTCHours()).padStart(2, "0");
+  const mins = String(date.getUTCMinutes()).padStart(2, "0");
+  const secs = String(date.getUTCSeconds()).padStart(2, "0");
+
+  return `${hours}:${mins}${showSeconds ? `:${secs}` : ""}` ;
+}
